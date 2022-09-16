4d3532173080dd761b70dfa4a35b5cea5467f7e8
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-09-17 00:03:07 +0530

    first commit :rocket:

---
+++ pages/Login.js
@@ -0,0 +1,66 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  
+  export default function SimpleCard() {
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bgGradient='repeating-linear(to-r, #1B2430, #153B44)'
+        >
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          {/* <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          </Stack> 
+          Heading
+          */}
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={'green.400'}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  bg={'green.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'green.500',
+                  }}>
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
---
+++ pages/Register.js
@@ -0,0 +1,99 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+  } from '@chakra-ui/react';
+  import { useState } from 'react';
+  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+  
+  export default function SignupCard() {
+    const [showPassword, setShowPassword] = useState(false);
+  
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bgGradient='repeating-linear(to-r, #1B2430, #153B44)'>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          {/* <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+              Sign up
+            </Heading>
+          </Stack> 
+          Heading
+          */}
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+                <Box>
+                  <FormControl id="fullName" isRequired>
+                    <FormLabel>Full Name</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+                </Box>
+                <HStack>
+                <Box>
+                  <FormControl id="mobile">
+                    <FormLabel>Mobile No</FormLabel>
+                    <Input type="tel" />
+                  </FormControl>
+                </Box>
+
+              <FormControl id="email" isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              </HStack>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? 'text' : 'password'} />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'green.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'green.500',
+                  }}>
+                  Register
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Already a user? <Link href='/Login' color={'green.400'}>Login</Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
